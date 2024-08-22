e695c313bde55c2cc2509aa99cabcb98e92fd202
Camilo <camiloagr2707@gmail.com>
2024-08-21 22:36:54 -0500

    Initial configuration

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,17 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	{ 'nvim-treesitter/nvim-treesitter-context' },
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function()
+			require('lualine').setup {
+				options = {
+					theme = 'dracula-nvim'
+				}
+			}
+		end
+	}
+}
